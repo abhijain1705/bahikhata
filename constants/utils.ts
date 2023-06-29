41b472f3698a8b8ecabdf7e5ea12aa40edfa3fc4
abhijain1705 <abhijain3002@gmail.com>
2023-06-29 23:38:51 +0530

    added some constant functions

--- constants/utils.ts
+++ constants/utils.ts
@@ -1,2 +1,21 @@
+import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
+import { CustLierUser } from "../common/interface/types";
+
 export const web_client_id =
   '1052516404612-g41a49tejeq70io83c4i7didgerckvmk.apps.googleusercontent.com';
+
+export function aggregate(a: CustLierUser[]) {
+  let finalObj: {[key: string]: CustLierUser[]} = {};
+  a.forEach(games => {
+    const timestamp =
+      games.accountCreatedDate as any as FirebaseFirestoreTypes.Timestamp;
+    let date = timestamp.toDate();
+    let dateString = date.toLocaleDateString();
+    if (finalObj[dateString]) {
+      finalObj[dateString].push(games);
+    } else {
+      finalObj[dateString] = [games];
+    }
+  });
+  return finalObj;
+}
