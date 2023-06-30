6d55b2df3258f6cd29609b19d78a51700d8916a3
abhijain1705 <abhijain3002@gmail.com>
2023-07-01 00:28:25 +0530

    adding filter to aggregate function

--- constants/utils.ts
+++ constants/utils.ts
@@ -1,12 +1,26 @@
-import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
-import { CustLierUser } from "../common/interface/types";
+import {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
+import {CustLierUser} from '../common/interface/types';
 
 export const web_client_id =
   '1052516404612-g41a49tejeq70io83c4i7didgerckvmk.apps.googleusercontent.com';
 
 export function aggregate(a: CustLierUser[]) {
+  const sortedArray = a.sort((x, y) => {
+    const dateA = (
+      x.accountCreatedDate as any as FirebaseFirestoreTypes.Timestamp
+    )
+      .toDate()
+      .getTime();
+    const dateB = (
+      y.accountCreatedDate as any as FirebaseFirestoreTypes.Timestamp
+    )
+      .toDate()
+      .getTime();
+    return dateB - dateA;
+  });
+
   let finalObj: {[key: string]: CustLierUser[]} = {};
-  a.forEach(games => {
+  sortedArray.forEach(games => {
     const timestamp =
       games.accountCreatedDate as any as FirebaseFirestoreTypes.Timestamp;
     let date = timestamp.toDate();
