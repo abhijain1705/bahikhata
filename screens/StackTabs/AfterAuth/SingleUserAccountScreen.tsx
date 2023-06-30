3bff34c2d74b51af5e401ef658674708547da597
abhijain1705 <abhijain3002@gmail.com>
2023-06-30 11:04:01 +0530

    added debit/credit buttons

--- screens/StackTabs/AfterAuth/SingleUserAccountScreen.tsx
+++ screens/StackTabs/AfterAuth/SingleUserAccountScreen.tsx
@@ -4,6 +4,7 @@ import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../../../common/interface/types';
 import Header from '../../../components/SingleUser/Header';
+import Button from '../../../common/components/button';
 
 const SingleUserAccountScreen = () => {
   const navigate = useNavigation<StackNavigationProp<RootStackParamList>>();
@@ -26,12 +27,52 @@ const SingleUserAccountScreen = () => {
   }, []);
 
   return (
-    <View>
+    <View style={styles.wrapper}>
       <Header wantCalls={true} custLierUser={custLierUser} />
+      <View style={styles.buttonWrapper}>
+        <Button
+          label={'Debit'}
+          onPress={() => {}}
+          loading={false}
+          color={'white'}
+          customBtnStyle={{
+            backgroundColor: 'red',
+            width: '40%',
+            padding: 0,
+            height: 60,
+          }}
+          customTextStyle={{fontWeight: '700', fontSize: 15, padding: 0}}
+        />
+        <Button
+          label={'Credit'}
+          onPress={() => {}}
+          loading={false}
+          color={'white'}
+          customBtnStyle={{
+            backgroundColor: 'green',
+            width: '40%',
+            padding: 0,
+            height: 60,
+          }}
+          customTextStyle={{fontWeight: '700', fontSize: 15, padding: 0}}
+        />
+      </View>
     </View>
   );
 };
 
 export default SingleUserAccountScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    position: 'relative',
+    justifyContent: 'space-between',
+  },
+  buttonWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+});
