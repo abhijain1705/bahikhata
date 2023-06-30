1f0702fec00ac34e16028534f9e9eb1bc3f60c87
abhijain1705 <abhijain3002@gmail.com>
2023-06-30 11:51:28 +0530

    added common alignment styles

---
+++ common/styles/styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const commonAlignment = StyleSheet.create({
+  centerAligned: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
