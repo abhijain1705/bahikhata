d08f4973a585ff0294fe11a9191d81bf8001e6e6
abhijain1705 <abhijain3002@gmail.com>
2023-07-03 17:33:59 +0530

    reduce padding from setting

--- components/Settings/settingBtn.tsx
+++ components/Settings/settingBtn.tsx
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 12,
+    padding: 10,
   },
   label: {
     color: '#222222',
